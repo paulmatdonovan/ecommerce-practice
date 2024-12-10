342f4f064d30b0543a516b899be2e48583533106
Paul Donovan <paul.m.donovan@outlook.com>
2024-12-11 10:39:20 +1100

    starts project

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons'
+import useStyles from './styles'
 
 const Product = ({ product }) => {
+
+    const classes = useStyles();
+
     return (
         <Card className={classes.root}>
             <CardMedia classname={classes.media} image="" title={product.name} />
@@ -17,6 +21,9 @@ const Product = ({ product }) => {
                 </div>
                 <Typography variant='h2' color='textSecondary'>{product.description}</Typography>
             </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label='Add to Cart' ><AddShoppingCart /></IconButton>
+            </CardActions >
         </Card>
     )
 }
